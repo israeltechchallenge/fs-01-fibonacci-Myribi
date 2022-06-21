cbf8d42ac7521d534ed688fe65a44a304527a7d1
Myribi <105745901+Myribi@users.noreply.github.com>
2022-06-21 19:28:07 +0300

    milestone 4

--- fibonacci.js
+++ fibonacci.js
@@ -1,68 +1,25 @@
+// function fibonacciRecursive(num) {
+//   if (num <= 1) return num;
+//   return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+// }
 
-const title = document.getElementById('title')
-
-function fibo(x, y) {
-    title.innerHTML = `The Fibonacci of ${x} is ${y}`;   
-}
-
-
-function fibonacci(num) {
-    let x = 0
-    let y = 1
-    let z;
-    let i = 0
-
-    for (i=2; i<=num; i++) {
-        z = x + y
-        x = y
-        y = z
-    }
-    return y;
+function callServer(number) {
+  fetch(`http://localhost:5050/fibonacci/${number}`)
+  .then((response) => response.json())
+    .then(data => {
+      document.getElementById("result").innerHTML = data["result"]
+    });
 }
 
 
 
-let x = 12;
-fibo(x,fibonacci(x))
+function inputChange() {
+  callServer(document.getElementById("number").value)
+}
+
+
+ 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function Xbonacci(sig,n) {
-   // var result = [];
-    //for (i=0; i<sig.length; i++) {
-       // result.push(sig[i]);
-   // }
-    //for (j=0; j<(n-sig.length); j++) {
-       // somme = 0
-       // for(k=0; k<sig.length; k++) {
-
-            //somme = somme + result[j+k]
-       // }
-    //result.push(somme)
-  //  }
-   // return result
-//}
-
-
-//var test = [1,1,1,1]
-//var long =10
-//console.log(Xbonacci(test,long))
+ 
