5850475dbe8ca783d05975894ab331861950330d
Myribi <105745901+Myribi@users.noreply.github.com>
2022-07-01 15:12:42 +0300

    Milestone 7.1

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,8 @@ const loader2 = document.getElementById("loader2");
 let btn = document.getElementById("btn");
 let result = document.getElementById("result");
 let checked = document.getElementById("flexCheckDefault");
-let menu = document.getElementById("menu");
+let dropdown = document.querySelectorAll(".dropdown-item");
+let history = document.getElementById("history");
 
 function removeSpinner() {
   loader.classList.remove("spinner-border");
@@ -14,25 +15,24 @@ function removeSpinner2() {
 
 function callServer(number) {
   fetch(`http://localhost:5050/fibonacci/${number}`)
-   .then((response) => {
+    .then((response) => {
       if (!response.ok) {
         response.text().then((erText) => {
           result.innerHTML = "Server Error:" + " " + erText;
           result.classList.add("text-danger");
           removeSpinner();
-          removeSpinner2()
-        });        
+          removeSpinner2();
+        });
       } else {
         return response.json();
-      }     
+      }
     })
     .then((data) => {
       if (data) {
         result.innerHTML = data["result"];
       }
       removeSpinner();
-      removeSpinner2()
-      
+      removeSpinner2();
     });
   callServerHistory();
 }
@@ -57,14 +57,14 @@ btn.addEventListener("click", function () {
     error.classList.remove("d-none");
     error.innerText = "Can't be larger than 50";
     removeSpinner();
-    removeSpinner2()
+    removeSpinner2();
     inputField.classList.add("text-danger", "border-danger");
     return;
   }
   if (checkbox.checked === false) {
     fibonacci();
     removeSpinner();
-    removeSpinner2()
+    removeSpinner2();
   } else {
     error.classList.remove("error");
     callServer(nums);
@@ -72,37 +72,26 @@ btn.addEventListener("click", function () {
   }
 });
 
-function callServerHistory() {
-  
+function callServerHistory(sort) {
   fetch("http://localhost:5050/getFibonacciResults")
     .then((response) => {
       return response.json();
-      
     })
 
     .then((data) => {
-      console.log(data);
+           
       if (document.getElementById("historyList") !== null) {
         document.getElementById("historyList").remove();
       }
 
-      let ol = document.createElement("ol");
-
-      ol.setAttribute("id", "historyList");
-      for (let i of data.results
-        .sort((a, b) => b["createdDate"] - a["createdDate"])
-        .slice(0, 5)) {
-        let li = document.createElement("li");
-        var date = new Date(i["createdDate"]);
-        li.innerHTML = `The Fibonacci of <b>${i["number"]}</b> is <b>${i["result"]}</b>. Calculated at: ${date}`;
-        ol.appendChild(li);
-
-        ol.classList.add("list-unstyled");
-        li.classList.add("fs-5", "border-bottom", "py-2", "border-secondary");
+      if (sort) {
+        sortBy(sort, data.results); 
       }
-      document.getElementById("history").appendChild(ol);
+
+      displayList(data.results)
     });
 }
+
 callServerHistory();
 
 function fibonacci() {
@@ -120,3 +109,54 @@ function fibonacci() {
   result.innerHTML = `<strong><u>${y}</u></strong>`;
 }
 
+dropdown.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    callServerHistory(event.target.innerText);
+  });
+});
+
+function sortBy(innerText, arraySorted) {
+  switch (innerText) {
+    case "Number Asc":
+      arraySorted.sort((a, b) => {
+        return a.number - b.number;
+      });
+      break;
+
+    case "Number Desc":
+      arraySorted.sort(function (a, b) {
+        return b.number - a.number;
+      });
+      break;
+
+    case "Date Asc":
+      arraySorted.sort(function (a, b) {
+        return a.createdDate - b.createdDate;
+      });
+      break;
+
+    case "Date Desc":
+      arraySorted.sort(function (a, b) {
+        return b.createdDate - a.createdDate;
+      });
+      break;
+  }
+}
+
+
+function displayList(listeArr) {
+  let ol = document.createElement("ol");
+
+  ol.setAttribute("id", "historyList");
+  history.innerHTML = "";
+  for (let i of listeArr) {
+    let li = document.createElement("li");
+    var date = new Date(i["createdDate"]);
+    li.innerHTML = `The Fibonacci of <b>${i["number"]}</b> is <b>${i["result"]}</b>. Calculated at: ${date}`;
+    ol.appendChild(li);
+
+    ol.classList.add("list-unstyled");
+    li.classList.add("fs-5", "border-bottom", "py-2", "border-secondary");
+    history.appendChild(ol)
+  }
+}
